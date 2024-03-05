fafd089f34ac1fa4510b465604f42438a232c68e
LinaBell <lina@topia.io>
2024-03-06 07:11:57 +1300

    Initial commit

---
+++ server/routes.js
@@ -0,0 +1,37 @@
+import express from "express";
+import {
+  handleDropAsset,
+  handleGetDroppedAssetsWithUniqueName,
+  handleGetWorldDetails,
+  handleGetDroppedAsset,
+  handleGetVisitor,
+  handleUpdateWorldDataObject,
+  moveVisitor,
+  handleRemoveDroppedAssets,
+} from "./controllers/index.js";
+import { getVersion } from "./utils/getVersion.js";
+
+const router = express.Router();
+
+router.get("/system/health", (req, res) => {
+  return res.json({
+    appVersion: getVersion(),
+    status: "OK",
+  });
+});
+
+// Dropped Assets
+router.get("/dropped-asset-with-unique-name", handleGetDroppedAssetsWithUniqueName);
+router.post("/dropped-asset", handleDropAsset);
+router.get("/dropped-asset", handleGetDroppedAsset);
+router.delete("/dropped-asset", handleRemoveDroppedAssets);
+
+// Visitor
+router.get("/visitor", handleGetVisitor);
+router.put("/visitor/move", moveVisitor);
+
+// World
+router.get("/world", handleGetWorldDetails);
+router.put("/world/data-object", handleUpdateWorldDataObject);
+
+export default router;
