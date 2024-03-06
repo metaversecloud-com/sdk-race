913499dda5f729d0f0e4775ba0d058b73bdb7cc1
Fabio Alexandrino <fabioalxk@users.noreply.github.com>
2024-03-05 21:55:47 -0300

    first commit

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -19,38 +19,8 @@ const Home = () => {
   };
 
   return (
-    <div className="container p-24 flex items-center justify-start">
-      <div className="flex flex-col">
-        <h1 className="h2 font-semibold">Server side example using interactive parameters</h1>
-        <div className="max-w-screen-lg">
-          {!hasInteractiveParams ? (
-            <p className="p1 my-4">
-              Edit an asset in your world and open the Links page in the Modify Asset drawer and add a link to your
-              website or use &quot;http://localhost:3000&quot; for testing locally. You can also add assetId,
-              interactiveNonce, interactivePublicKey, urlSlug, and visitorId directly to the URL as search parameters to
-              use this feature.
-            </p>
-          ) : (
-            <p className="p1 my-4">Interactive parameters found, nice work!</p>
-          )}
-        </div>
-
-        <button className="btn w-fit" onClick={handleGetDroppedAsset}>
-          Get Dropped Asset Details
-        </button>
-        {droppedAsset && (
-          <div className="flex flex-col w-full items-start">
-            <p className="p1 mt-4 mb-2">
-              You have successfully retrieved the dropped asset details for {droppedAsset.assetName}!
-            </p>
-            <img
-              className="w-96 h-96 object-cover rounded-2xl my-4"
-              alt="preview"
-              src={droppedAsset.topLayerURL || droppedAsset.bottomLayerURL}
-            />
-          </div>
-        )}
-      </div>
+    <div className="app-wrapper">
+      test
     </div>
   );
 };
