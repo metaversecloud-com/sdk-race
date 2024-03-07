38ceccee0461d1b7bf1dca9ba4d5b944f9f3d3fa
Fabio Alexandrino <fabioalxk@users.noreply.github.com>
2024-03-07 12:17:01 -0300

    on your mark screen

--- client/src/components/OnYourMarkScreen.jsx
+++ client/src/components/OnYourMarkScreen.jsx
@@ -1,26 +1,41 @@
 import React, { useState, useEffect } from "react";
+import "./OnYourMarkScreen.scss";
 
 const OnYourMarkScreen = () => {
-  const [countdown, setCountdown] = useState(["On Your Mark...", "Get Set...", "3...", "2...", "1...", "Run!"]);
+  const countdown = ["3...", "2...", "1..."];
   const [currentMessage, setCurrentMessage] = useState(0);
+  const initialMessages = ["On Your Mark...", "Get Set..."];
+  const [animationKey, setAnimationKey] = useState(Date.now());
 
   useEffect(() => {
-    if (currentMessage < countdown.length - 1) {
+    if (currentMessage < countdown.length) {
       const timerId = setTimeout(() => {
         setCurrentMessage(currentMessage + 1);
+        setAnimationKey(Date.now());
       }, 1000);
       return () => clearTimeout(timerId);
-    } else {
-      startRace(); // new
     }
-  }, [currentMessage, countdown]);
+  }, [currentMessage]);
 
-  const startRace = () => {
-    // Placeholder function to start the race
-    // TODO: Implement the race start logic here
-  };
+  const isRunDisplayed = currentMessage === countdown.length;
 
-  return <div>{countdown[currentMessage]}</div>;
+  return (
+    <div className="container">
+      <div style={{ paddingTop: "300px", paddingBottom: "50px" }}>
+        {initialMessages.map((message, index) => (
+          <div key={index} className="initialMessage">
+            {message}
+          </div>
+        ))}
+      </div>
+      <div
+        key={isRunDisplayed ? "run" : animationKey}
+        className={`countdown ${!isRunDisplayed ? "small-to-large" : "heartbeat"}`}
+      >
+        {isRunDisplayed ? "Run!" : countdown[currentMessage]}
+      </div>
+    </div>
+  );
 };
 
 export default OnYourMarkScreen;
