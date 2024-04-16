e99430ff26ee43608bc082609c696940e7cb5d10
Fabio Alexandrino <fabioalxk@users.noreply.github.com>
2024-04-16 15:30:30 -0300

    time and toast fix

--- server/controllers/race/handleCompleteRace.js
+++ server/controllers/race/handleCompleteRace.js
@@ -1,32 +1,29 @@
 import { Visitor, World, DroppedAsset } from "../../utils/topiaInit.js";
 import { errorHandler } from "../../utils/index.js";
 
-/* This functions completes the race
- * Reset the game for that user
- * Update the leaderboard with the elapsed time of the user
- */
+// Função auxiliar para converter o tempo de string para valor numérico
+function timeToValue(timeString) {
+  const [minutes, seconds] = timeString.split(":");
+  return parseInt(minutes, 10) * 60 + parseInt(seconds, 10);
+}
+
 export const handleCompleteRace = async (req, res) => {
   try {
     console.log("handleCompleteRace");
     const { interactiveNonce, interactivePublicKey, urlSlug, visitorId, assetId, profileId, username } = req.query;
     const { elapsedTime } = req.body;
-
-    const credentials = {
-      interactiveNonce,
-      interactivePublicKey,
-      visitorId,
-      assetId,
-    };
+    const credentials = { interactiveNonce, interactivePublicKey, visitorId, assetId };
 
     if (!elapsedTime) {
       return res.status(400).json({ error: "Elapsed time is required" });
     }
 
+    const visitor = await Visitor.create(visitorId, urlSlug, { credentials });
     const world = await World.create(urlSlug, { credentials });
     await world.fetchDataObject();
 
     if (!world?.dataObject?.race) {
-      world.dataObject.race == {};
+      world.dataObject.race = {};
     }
 
     if (!world?.dataObject?.race?.leaderboard) {
@@ -34,14 +31,29 @@ export const handleCompleteRace = async (req, res) => {
       await world.updateDataObject({ race });
     }
 
-    world.dataObject.race.leaderboard[profileId] = { username, elapsedTime };
+    const currentBestTime = world.dataObject.race.leaderboard?.[profileId]?.elapsedTime;
+
+    if (!currentBestTime || timeToValue(elapsedTime) < timeToValue(currentBestTime)) {
+      world.dataObject.race.leaderboard[profileId] = { username, elapsedTime };
+      world
+        .updateDataObject({
+          [`race.profiles.${profileId}`]: null,
+          [`race.leaderboard.${profileId}`]: { username, elapsedTime },
+        })
+        .then()
+        .catch();
+    }
 
     const leaderboard = world.dataObject.race.leaderboard;
 
-    await world.updateDataObject({
-      [`race.profiles.${profileId}`]: null,
-      [`race.leaderboard.${profileId}`]: { username, elapsedTime },
-    });
+    visitor
+      .fireToast({
+        groupId: "race",
+        title: "Finish",
+        text: `You finished the race! Your time: ${elapsedTime}`,
+      })
+      .then()
+      .catch();
 
     return res.json({ success: true, leaderboard, elapsedTime });
   } catch (error) {
