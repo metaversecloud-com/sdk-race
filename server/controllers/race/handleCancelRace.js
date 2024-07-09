6c4ec11806850fc94ba15647d04ff8f31b852b35
Fabio Alexandrino <fabioalxk@users.noreply.github.com>
2024-07-09 18:06:19 -0300

    cancel race fix

--- server/controllers/race/handleCancelRace.js
+++ server/controllers/race/handleCancelRace.js
@@ -3,7 +3,7 @@ import { errorHandler } from "../../utils/index.js";
 
 export const handleCancelRace = async (req, res) => {
   try {
-    const { interactiveNonce, interactivePublicKey, urlSlug, visitorId, assetId, profileId } = req.query;
+    const { interactiveNonce, interactivePublicKey, urlSlug, visitorId, assetId, profileId, sceneDropId } = req.query;
 
     const credentials = {
       interactiveNonce,
