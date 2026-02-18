42bbdd5707b0fbefd94f7d600e0964fad7d24369
Lina <linabell@mail.com>
2026-02-17 19:38:04 -0800

    update build paths

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,11 @@
-import express, { Request, Response, NextFunction } from "express";
+import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import path from "path";
 import router from "./routes.js";
-import { getVersion } from "./utils/getVersion.js";
-
-const app = express();
-
+import path from "path";
 import { cleanReturnPayload } from "./utils/cleanReturnPayload.js";
 import { fileURLToPath } from "url";
+
 dotenv.config({ path: "../.env" });
 
 function checkEnvVariables() {
@@ -18,22 +15,42 @@ function checkEnvVariables() {
   if (missingVariables.length > 0) {
     throw new Error(`Missing required environment variables in the .env file: ${missingVariables.join(", ")}`);
   } else {
-    console.info("All required environment variables provided.");
+    console.log("All required environment variables provided.");
   }
 }
 checkEnvVariables();
 
 const PORT = process.env.PORT || 3000;
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use("/api", router);
 
-app.use(cors());
+if (process.env.NODE_ENV === "development") {
+  const corsOptions = {
+    origin: ["http://localhost:3000", "http://localhost:5173"],
+    credentials: true, //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+  };
+  app.use(cors(corsOptions));
+} else {
+  // Node serves the files for the React app
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  app.use(express.static(path.resolve(__dirname, "../../../client/build")));
 
-app.use(function (req: Request, res: Response, next: NextFunction) {
+  // All other GET requests not handled before will return our React app
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../../../client/build", "index.html"));
+  });
+}
+
+app.use(function (req, res, next) {
   const ogSend = res.send;
-  res.send = function (data: any) {
+  // @ts-ignore
+  res.send = function (data) {
     if (data) {
       try {
         const cleanData = cleanReturnPayload(typeof data === "string" ? JSON.parse(data) : data);
@@ -44,24 +61,10 @@ app.use(function (req: Request, res: Response, next: NextFunction) {
         next();
       }
     }
-  } as any;
+  };
   next();
 });
 
-app.use("/api", router);
-
-if (process.env.NODE_ENV !== "development") {
-  // Node serves the files for the React app
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = path.dirname(__filename);
-  app.use(express.static(path.resolve(__dirname, "../client/build")));
-
-  // All other GET requests not handled before will return our React app
-  app.get("*", (req: Request, res: Response) => {
-    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
-  });
-}
-
 app.listen(PORT, () => {
-  console.info(`Server is running on port: ${PORT}, version: ${getVersion()}`);
+  console.log(`Server is running on port: ${PORT}`);
 });
